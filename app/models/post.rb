af2ac9b58fe96762896fa149039383f8025ffd77
KeiFunahashi <fnhskei@yahoo.co.jp>
2019-11-19 18:35:02 +0900

    view変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,5 +9,6 @@ belongs_to :user
     Post.where('title LIKE(?)', "%#{search}%")
     Post.where('text LIKE(?)', "%#{search}%")
     Post.where('place LIKE(?)', "%#{search}%")
+    Post.where('time LIKE(?)', "%#{search}%")
     end
   end
