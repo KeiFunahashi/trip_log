e525ed6b5a29ca470e95d55e647de851654d7843
KeiFunahashi <fnhskei@yahoo.co.jp>
2020-01-12 19:10:52 +0900

    deployバグ修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,6 @@ set :repo_url,  'git@github.com:KeiFunahashi/trip_log.git'
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 # set :linked_files, fetch(:linked_files, []).push("config/master.key")
-set :linked_files, %w{ config/credentials.yml.enc }
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' 
 
@@ -27,11 +26,11 @@ set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
 # デプロイ処理が終わった後、Unicornを再起動するための記述
-after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
-  end
+# after 'deploy:publishing', 'deploy:restart'
+# namespace :deploy do
+#   task :restart do
+#     invoke 'unicorn:restart'
+#   end
   
 #   desc 'upload master.key'#ここ注意
 #   task :upload do
@@ -46,15 +45,22 @@ namespace :deploy do
 #   after :finishing, 'deploy:cleanup'
 # end
 
-desc 'upload credentials.yml.enc'
-task :upload do
-  on roles(:app) do |host|
-    if test "[ ! -d #{shared_path}/config ]"
-      execute "mkdir -p #{shared_path}/config"
-    end
-    upload!('config/credentials.yml.enc', "#{shared_path}/config/credentials.yml.enc")
+set :linked_files, %w{ config/master.key }
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
   end
-end
-before :starting, 'deploy:upload'
-after :finishing, 'deploy:cleanup'
+
+  desc 'upload master.key'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/master.key', "#{shared_path}/config/master.key")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
