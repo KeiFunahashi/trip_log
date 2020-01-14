58ce971edbef4a642a0f825b91defa6ac7e8bf81
KeiFunahashi <fnhskei@yahoo.co.jp>
2020-01-14 18:14:37 +0900

    投稿者プロフィール画面へのパス追加

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,9 +1,18 @@
 class ProfilesController < ApplicationController
+  before_action :move_to_index
+
   def show
     # @users = User.all
     @users = User.find(params[:id])
     @posts = Post.where(user_id:params[:id])
   end
+
+  private
+  
+  def move_to_index
+    redirect_to new_user_session_path unless user_signed_in?
+  end
+
 end
 
 
